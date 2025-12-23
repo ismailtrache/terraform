76d0be76ca01da6e5b5d3fc84b4714a42bebfe85
ismailtrache <ismailtrache2@gmail.com>
2025-12-23 05:59:24 -0500

    Initial Terraform AWS project

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.92"
+    }
+  }
+
+  required_version = ">= 1.2"
+}
+
+provider "aws" {
+  region = "ca-central-1"
+}
+
+
+resource "aws_instance" "monserveur" {
+  ami           = "ami-085f043560da76e08"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "serveurInstance"
+  }
+}
+
+resource "aws_vpc" "vpc_toronto_test" {
+  cidr_block       = "10.10.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "vpc_toronto_test"
+  }
+}
